c6f9eba6b327c07ec406183c57d8051dd5b0f113
rzfzr <capitao.rafa@gmail.com>
2026-01-24 11:22:13 +0100

    Add plot_dpg_constraints_overview function

--- dpg/__init__.py
+++ dpg/__init__.py
@@ -1,11 +1,12 @@
 # dpg/__init__.py
 from .core import DecisionPredicateGraph
-from .visualizer import plot_dpg, plot_dpg_reg
+from .visualizer import plot_dpg, plot_dpg_reg, plot_dpg_constraints_overview
 
 __all__ = [
     "DecisionPredicateGraph",
     "plot_dpg",
     "plot_dpg_reg",
+    "plot_dpg_constraints_overview",
     "extract_graph_metrics",
     "extract_node_metrics",
 ]
